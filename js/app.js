f824411c0edfddfb3358eac9b14f9a35937cd83d
pietrocruciata <cruciata9748@gmail.com>
2024-03-14 15:14:41 +0100

    fix: cursore none

--- js/app.js
+++ js/app.js
@@ -5,15 +5,31 @@ const inputElementDom = document.querySelector('.input')
 
 //CREARE L'EVENTO SU QUEL BOTTONE
 createGrid.addEventListener('click', function () {
-
     //CREARE LA GRIGLIA
     //CREARE LE CELLE
     const gridElement = document.querySelector('.grid');
+    gridElement.classList.remove('cursore-none');
+
     gridElement.innerHTML = ''
     let size;
 
     const min = 1;
-    let max = 100;
+    let max;
+
+    //PER OGNI DIFFICOLà GESTIRE LE CELLE PRESENTI IN MANIERA ORDINATA
+    if (inputElementDom.value === 'easy') {
+        size = 10;
+        max = 100
+        gridElement.classList.add('easy')
+    } else if (inputElementDom.value === 'medium') {
+        size = 9;
+        max = 81
+        gridElement.classList.add('medium')
+    } else {
+        size = 7;
+        max = 49
+        gridElement.classList.add('hard')
+    }
     const randomNumbers = 16;
 
     const arrayBombs = [];
@@ -28,17 +44,7 @@ createGrid.addEventListener('click', function () {
 
     }
     console.log(arrayBombs);
-    //PER OGNI DIFFICOLà GESTIRE LE CELLE PRESENTI IN MANIERA ORDINATA
-    if (inputElementDom.value === 'easy') {
-        size = 10;
-        gridElement.classList.add('easy')
-    } else if (inputElementDom.value === 'medium') {
-        size = 9;
-        gridElement.classList.add('medium')
-    } else {
-        size = 7;
-        gridElement.classList.add('hard')
-    }
+
     const numberOfCelles = size * size;
 
 
@@ -53,27 +59,26 @@ createGrid.addEventListener('click', function () {
         cellElement.innerHTML = num
         gridElement.append(cellElement)
 
-
-let score = document.querySelector('.score')
-    score.innerHTML = 0
+        //CREARE UN COUNTER DI CLICK SULLE CELLE NON BOMBE
+        let score = document.querySelector('.score')
+        score.innerHTML = 0
 
 
         // AD OGNI CLICK LO SFONDO DEVE ESSERE AZZURRO
         cellElement.addEventListener('click', function () {
             cellElement.classList.add('bg-lightblue')
             score.innerHTML++
-
-
             
-
             for (let i = 0; i < arrayBombs.length; i++) {
                 const bombcell = arrayBombs[i]
+
                 if (parseInt(cellElement.innerHTML) === bombcell) {
                     //COLORARE DI ROSSO LE CELLE CLICCATE DOVE PRESENTE LA BOMBA
 
                     cellElement.classList.remove('bg-lightblue')
                     cellElement.classList.add('bg-red')
-                    //CREARE UN COUNTER DI CLICK SULLE CELLE NON BOMBE
+                    gridElement.classList.add('cursore-none')
+                    score.innerHTML--
                 }
             }
 
