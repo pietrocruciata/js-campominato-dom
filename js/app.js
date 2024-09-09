10f43ce40073472ac8d72f8cf32ee45445ea2ce1
pietrocruciata <cruciata9748@gmail.com>
2024-09-09 12:03:55 +0200

    add foto

--- js/app.js
+++ js/app.js
@@ -1,40 +1,10 @@
-//COLLEGARE IL BOTTONE PER FORMARE LA GRIGLIA
-const createGrid = document.querySelector('.button');
-//COLLEGARE LA SELECT
-const inputElementDom = document.querySelector('.input')
-
-//CREARE L'EVENTO SU QUEL BOTTONE
-createGrid.addEventListener('click', function () {
-    //CREARE LA GRIGLIA
-    //CREARE LE CELLE
-    const gridElement = document.querySelector('.grid');
-    gridElement.classList.remove('cursore-none');
-
-    gridElement.innerHTML = ''
-    let size;
-
-    const min = 1;
-    let max;
-
-    //PER OGNI DIFFICOLà GESTIRE LE CELLE PRESENTI IN MANIERA ORDINATA
-    if (inputElementDom.value === 'easy') {
-        size = 10;
-        max = 100
-        gridElement.classList.add('easy')
-    } else if (inputElementDom.value === 'medium') {
-        size = 9;
-        max = 81
-        gridElement.classList.add('medium')
-    } else {
-        size = 7;
-        max = 49
-        gridElement.classList.add('hard')
-    }
-    const randomNumbers = 16;
-
+//
+// UTILITY
+//
+function generateBombs(bombNumbers, max, min) {
     const arrayBombs = [];
 
-    while (arrayBombs.length < randomNumbers) {
+    while (arrayBombs.length < bombNumbers) {
         //GENERARE 16 NUMERI CASUALI UNICI
         const bombRandomNumbers = parseInt(Math.floor(Math.random() * max) + min)
 
@@ -44,6 +14,48 @@ createGrid.addEventListener('click', function () {
 
     }
     console.log(arrayBombs);
+    return arrayBombs;
+}
+
+//
+// MAIN
+//
+
+//COLLEGARE IL BOTTONE PER FORMARE LA GRIGLIA
+const createGrid = document.querySelector('.button');
+//COLLEGARE LA SELECT
+const inputElementDom = document.querySelector('.input');
+
+//CREARE L'EVENTO SU QUEL BOTTONE
+createGrid.addEventListener('click', function () {
+    //CREARE LA GRIGLIA
+    //CREARE LE CELLE
+    const gridElement = document.querySelector('.grid');
+    gridElement.classList.remove('cursore-none');
+
+    gridElement.innerHTML = '';
+    let size;
+
+    const minNum = 1;
+    let maxNum;
+
+    //PER OGNI DIFFICOLà GESTIRE LE CELLE PRESENTI IN MANIERA ORDINATA
+    if (inputElementDom.value === 'easy') {
+        size = 10;
+        maxNum = 100
+        gridElement.classList.add('easy');
+    } else if (inputElementDom.value === 'medium') {
+        size = 9;
+        maxNum = 81
+        gridElement.classList.add('medium');
+    } else {
+        size = 7;
+        maxNum = 49
+        gridElement.classList.add('hard');
+    }
+    const randomNumbers = 16;
+
+    const arrayBombs = generateBombs(randomNumbers, maxNum, minNum);
 
     const numberOfCelles = size * size;
 
@@ -55,30 +67,40 @@ createGrid.addEventListener('click', function () {
 
         // AGGIUNGERE LE CELLE AL DOM
         const cellElement = document.createElement('div')
-        cellElement.className = 'cell'
-        cellElement.innerHTML = num
-        gridElement.append(cellElement)
+        cellElement.className = 'cell';
+        cellElement.innerHTML = num;
+
+        if (arrayBombs.includes(num) === true) {
+            console.log(num + ' è una bomba');
+        }
+
+        gridElement.append(cellElement);
+
 
         //CREARE UN COUNTER DI CLICK SULLE CELLE NON BOMBE
         let score = document.querySelector('.score')
-        score.innerHTML = 0
+        score.innerHTML = 0;
 
 
         // AD OGNI CLICK LO SFONDO DEVE ESSERE AZZURRO
         cellElement.addEventListener('click', function () {
             cellElement.classList.add('bg-lightblue')
-            score.innerHTML++
-            
+            score.innerHTML++;
+
             for (let i = 0; i < arrayBombs.length; i++) {
-                const bombcell = arrayBombs[i]
+                const bombcell = arrayBombs[i];
 
-                if (parseInt(cellElement.innerHTML) === bombcell) {
-                    //COLORARE DI ROSSO LE CELLE CLICCATE DOVE PRESENTE LA BOMBA
+                if (num === bombcell) { // Se il numero che ho cliccato è una bomba
 
-                    cellElement.classList.remove('bg-lightblue')
-                    cellElement.classList.add('bg-red')
-                    gridElement.classList.add('cursore-none')
-                    score.innerHTML--
+                    //COLORARE DI ROSSO TUTTE LE CELLE CLICCATE DOVE PRESENTE LA BOMBA
+                    for (let i = 0; i < arrayBombs.length; i++) {
+
+                        cellElement.classList.remove('bg-lightblue');
+                        cellElement.classList.add('bg-red');
+                    }
+
+                    gridElement.classList.add('cursore-none');
+                    score.innerHTML--;
                 }
             }
 
